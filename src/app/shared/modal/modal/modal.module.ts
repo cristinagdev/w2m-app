23d2a13a2c402c4a933807b243c94450b37b8f1c
Cristina Garcia <cgarciasan@hiberus.com>
2023-04-12 12:55:55 +0200

    feat: added modal component

---
+++ src/app/shared/modal/modal/modal.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModalComponent } from './modal.component';
+import { MaterialModule } from 'src/app/material.module';
+
+
+
+@NgModule({
+  declarations: [
+    ModalComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule
+  ],
+  exports:[ ModalComponent]
+})
+export class ModalModule { }
