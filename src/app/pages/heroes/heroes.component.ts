bd7f9243bc0e1ab35235eecbfe75138d63b2d2a9
Cristina Garcia <cgarciasan@hiberus.com>
2023-04-14 15:02:33 +0200

    fix: fix search filter

--- src/app/pages/heroes/heroes.component.ts
+++ src/app/pages/heroes/heroes.component.ts
@@ -1,11 +1,12 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
-import { debounceTime, Observable } from 'rxjs';
+import { debounceTime,  Observable, switchMap } from 'rxjs';
 import { ModalComponent } from 'src/app/shared/modal/modal.component';
+import { SpinnerService } from 'src/app/shared/spinner/services/spinner.service';
 import { Hero } from './interfaces/hero.interface';
 import { HeroService } from './services/hero.service';
 
@@ -15,19 +16,38 @@ import { HeroService } from './services/hero.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent implements AfterViewInit {
+export class HeroesComponent implements OnInit, AfterViewInit {
 
   search: FormControl= new FormControl()
   heroesList$!: Observable<Hero[]>;
   heroesList: Hero[] = [];
+  languageSelected: string='';
+
 
   displayedColumns: string[] = ['position', 'name', 'power','button', ];
   dataSource: MatTableDataSource<Hero>= new MatTableDataSource(this.heroesList);
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
 
-  constructor ( private heroService: HeroService, private router: Router, public dialog: MatDialog) {
+  constructor (
+    private heroService: HeroService,
+    private router: Router,
+    public dialog: MatDialog,
+    private spinnerService: SpinnerService
+    ) {
      this.getHeroesList();
+
+  }
+
+  ngOnInit(): void {
+    this.search.valueChanges.pipe(
+      debounceTime(500),
+      switchMap(changedValue => this.heroService.searchHero(changedValue)),
+   ).subscribe(value => {
+    this.dataSource.data= value;
+
+   })
+
   }
 
   ngAfterViewInit(): void {
@@ -42,17 +62,6 @@ export class HeroesComponent implements AfterViewInit {
       this.heroesList= res;
       this.dataSource.data= this.heroesList;
     });
-
-  }
-
-  searchHero(){
-    this.search.valueChanges.pipe(
-      debounceTime(500)
-    ).subscribe((value)=> {
-       this.heroService.searchHero(value).subscribe((response) => {
-          this.heroesList = response;
-    })
-    })
 
   }
 
@@ -75,7 +84,6 @@ export class HeroesComponent implements AfterViewInit {
     })
 
   }
-
 
 
 
