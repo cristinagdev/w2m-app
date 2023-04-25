5f2821d439639a3cb3ce172083a472ef01caca37
Cristina Garcia <cgarciasan@hiberus.com>
2023-04-25 13:11:30 +0200

    fix: fix spinner subject and change spinner module for material module

--- src/app/shared/spinner/services/spinner.service.ts
+++ src/app/shared/spinner/services/spinner.service.ts
@@ -2,19 +2,19 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SpinnerService {
+  private isLoading: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  isLoading$ = this.isLoading.asObservable();
 
-  isLoading$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  constructor() {}
 
-  constructor() { }
-
-  show(): void{
-    this.isLoading$.next(true)
+  show(): void {
+    this.isLoading.next(true);
   }
 
-  hide(): void{
-    this.isLoading$.next(false)
+  hide(): void {
+    this.isLoading.next(false);
   }
 }
