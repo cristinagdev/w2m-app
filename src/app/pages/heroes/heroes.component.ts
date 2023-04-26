a038c2765a35bfdd397917d0479661426314553d
Cristina Garcia <cgarciasan@hiberus.com>
2023-04-26 14:21:33 +0200

    style: add some style to buttons

--- src/app/pages/heroes/heroes.component.ts
+++ src/app/pages/heroes/heroes.component.ts
@@ -4,80 +4,70 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
-import { debounceTime,  Observable, switchMap } from 'rxjs';
+import { debounceTime, finalize, Observable, switchMap } from 'rxjs';
 import { ModalComponent } from 'src/app/shared/modal/modal.component';
 import { SpinnerService } from 'src/app/shared/spinner/services/spinner.service';
 import { Hero } from './interfaces/hero.interface';
 import { HeroService } from './services/hero.service';
 
-
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit, AfterViewInit {
-
-  search: FormControl= new FormControl()
+  search: FormControl = new FormControl();
   heroesList: Hero[] = [];
 
-  displayedColumns: string[] = [ 'name', 'ability','button', ];
-  dataSource: MatTableDataSource<Hero>= new MatTableDataSource(this.heroesList);
+  displayedColumns: string[] = ['name', 'ability', 'button'];
+  dataSource: MatTableDataSource<Hero>;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-
-  constructor (
+  constructor(
     private heroService: HeroService,
     private router: Router,
-    public dialog: MatDialog,
-    ) {
-     this.getHeroesList();
-
+    public dialog: MatDialog
+  ) {
+    this.dataSource = new MatTableDataSource(this.heroesList);
   }
 
   ngOnInit(): void {
-    this.search.valueChanges.pipe(
-      debounceTime(500),
-      switchMap(changedValue => this.heroService.searchHero(changedValue)),
-   ).subscribe(value => {
-    this.dataSource.data= value;
-
-   })
-
+    this.getHeroesList();
+    this.search.valueChanges
+      .pipe(
+        debounceTime(500),
+        switchMap((changedValue) => this.heroService.searchHero(changedValue))
+      )
+      .subscribe((value) => {
+        this.dataSource.data = value;
+      });
   }
 
   ngAfterViewInit(): void {
-    this.dataSource.paginator= this.paginator;
+    this.dataSource.paginator = this.paginator;
   }
 
   getHeroesList(): void {
     this.heroService.getListHeroes().subscribe((res) => {
-      this.dataSource.data= res;
+      this.dataSource.data = res;
     });
-
   }
 
-
-
-  deleteHero(id: number): void{
-    const dialogRef = this.dialog.open(ModalComponent)
-
-    dialogRef.afterClosed().subscribe((res)=> {
-      if(res) {
-        this.heroService.deleteHero(id).subscribe()
+  deleteHero(id: number): void {
+    const dialogRef = this.dialog.open(ModalComponent);
+    dialogRef.afterClosed().subscribe((res) => {
+      if (res) {
+        this.heroService
+          .deleteHero(id)
+          .pipe(finalize(() => this.getHeroesList()))
+          .subscribe();
       }
-      this.getHeroesList()
-    })
-
+    });
   }
 
-  updateHero(id: number): void{
+  updateHero(id: number): void {
     this.router.navigate(['/heroes/create'], {
-      queryParams: {id: id}
-    })
-
+      queryParams: { id: id },
+    });
   }
-
-
-
 }
